631f89ec591f42711f307d45a47401f494be46aa
Julián Parra <santiagoleal807@gmail.com>
2026-05-20 20:54:51 -0500

    Add files via upload

---
+++ sql/setup.sql
@@ -0,0 +1,18 @@
+-- =============================================
+-- Doom Telemetry Database
+-- setup.sql - Recreacion completa de la DB
+-- Ejecutar con:
+-- psql -U postgres -d doom_telemetry -f setup.sql
+-- =============================================
+
+-- 1. Schema: creacion de tablas
+\i sql/01_schema.sql
+
+-- 2. ETL: carga de sesiones reales desde TSV
+\i sql/02_etl.sql
+
+-- 3. Instrumento UX: preguntas BANGS
+\i sql/03_ux_instrument_bangs.sql
+
+-- 4. Datos sinteticos: 6 jugadores, 3 episodios, 20k+ filas
+\i sql/05_sample_data.sql
